6ccfb9c44cf4c5527dfe12ec3dba4a2b5eecc950
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2020-06-28 18:29:20 +0200

    Years pattern added to config

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 DATA_PATH = 'data'
 PRETRAINED_MODEL_PATH = 'lid.176.bin'
-keywords = ["influenza", "flu", "epidemic", "pandemic"]
+keywords = ["influenza", " flu", "epidemic", "pandemic"]
 citationfile = 'citations.txt'
+years = '^(1919|1918|192\d)'
 DEBUG = False
 languages = ['en']
